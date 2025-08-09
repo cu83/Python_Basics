8446f50c3ff03f39463fa960c4205a7ce472e135
Engr. Matthew <matthewloquinerioxx@gmail.com>
2025-08-09 23:23:48 +0800

    Added Test File

---
+++ src/test_cap.py
@@ -0,0 +1,16 @@
+import unittest
+import cap
+
+class TestCap(unittest.TestCase):
+    def test_one_word(self):
+        text = 'python'
+        result = cap.cap(text)
+        self.assertEqual(result, 'Python')
+
+    def multiple_words(self):
+        text = 'monty python'
+        result = cap.cap(text)
+        self.assertEqual(result, 'Monty python')
+
+if __name__ == '__main__':
+    unittest.main()
